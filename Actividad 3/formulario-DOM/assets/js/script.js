0ca383396bd4a71d488d443f44d837b407342733
Shleideric <leonardo.gutial@gmail.com>
2025-03-20 16:30:03 -0300

    Modificación a ultima Actividad Realizada

--- Actividad 3/formulario-DOM/assets/js/script.js
+++ Actividad 3/formulario-DOM/assets/js/script.js
@@ -1,23 +1,23 @@
-
-
 let botonEnviar = document.getElementById("boton-enviar");
-botonEnviar.textContent = "Reserva Ahora";
+botonEnviar.textContent ="Reserva Ahora";
 
-botonEnviar.addEventListener(`click`, function(){
+botonEnviar.addEventListener('click', function(){
     let nombre = document.getElementById('nombre').value;
     let fechaLlegada = document.getElementById('fecha-llegada').value;
+    let fechaSalida = document.getElementById('fecha-salida').value;
     let tipoHabitacion = document.getElementById('tipo-habitacion').value;
-    if (nombre && fechaLlegada && tipoHabitacion) {
-        alert(`Reserva confirmada a nombre de: #(nombre) para la fecha de llegada #(fechaLlegada) y su tipo de habitacion es #(tipoHabitacion)`);
-    } else {
+    if (nombre && fechaLlegada && tipoHabitacion && fechaSalida) {
+        alert(`Reserva confirmada a nombre de: ${nombre} para la fecha de llegada ${fechaLlegada} y su tipo de habitacion es ${tipoHabitacion}`);
+    }
+    else{
         alert(`Por favor complete el formulario en su totalidad`);
     }
 })
 
-botonEnviar.addEventListener("mouseover", () -> {
-    botonEnviar.style.backgroundColor ="red"
+botonEnviar.addEventListener("mouseover", () => {
+    botonEnviar.style.backgroundColor ="pink";
 })
 
-botonEnviar.addEventListener("mouseover", () -> {
-    botonEnviar.style.backgroundColor = "gray"
+botonEnviar.addEventListener("mouseout", () => {
+    botonEnviar.style.backgroundColor="palevioletred";
 })
